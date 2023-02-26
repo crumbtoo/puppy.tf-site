33e79e35d36efb5c9eb72dbaf2546268ee318b9e
crumbtoo <crumb@duck.com>
2023-02-26 03:31:23 -0700

    whatever

--- src/Main.elm
+++ src/Main.elm
@@ -1,58 +1,64 @@
 module Main exposing (main)
-
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Url
 
-
-main =
-    Browser.sandbox { init = init, update = update, view = view }
+main : Program () Model Msg
+main = Browser.application
+       { init = init
+       , view = view
+       , update = update
+       , subscriptions = (\_ -> Sub.none)
+       , onUrlChange = UrlChanged
+       , onUrlRequest = LinkClicked
+       }
 
 type alias Model =
-    { tabno : Int
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+type Msg = LinkClicked Browser.UrlRequest
+         | UrlChanged Url.Url
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key = (Model key url, Cmd.none)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "URL Interceptor"
+    , body =
+        [ text "The current URL is: "
+        , b [] [ text (Url.toString model.url) ]
+        , ul []
+            [ viewLink "#scripts"
+            , viewLink "#binds"
+            , viewLink "#blocks"
+            ]
+        ]
     }
 
 
-init : Model
-init = Model 0
-
-type Msg = TabChange Int
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        TabChange tabno ->
-            { model | tabno = tabno }
-
-view : Model -> Html Msg
-view model =
-    div []
-    [ div [ class "makecfg-tabs" ]
-      [ viewTabButton 0
-      , viewTabButton 1
-      , viewTabButton 2
-      , viewTabButton 3
-      , viewTab model.tabno
-      ]
-    ]
-
-viewTabButton : Int -> Html Msg
-viewTabButton newtabno =
-    input
-    [ type_ "radio"
-    , name "tabs"
-    , value (String.fromInt newtabno)
-    , onClick ()
-    , checked (if newtabno == 0 then True else False)
-    ]
-    []
-
-viewTab : Int -> Html msg
-viewTab tabno =
-    case tabno of
-        0 -> div [] [ text "hi" ]
-        1 -> div [] [ text "hello" ]
-        2 -> div [] [ text "frrrm" ]
-        _ -> div [] [ text "naauurr" ]
+viewLink : String -> Html msg
+viewLink path =
+    li [] [ a [ href path ] [ text path ] ]
 
