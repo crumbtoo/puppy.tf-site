15251a7294facbfbd31dfa2564add239e25a153e
Madeline Slaga <crumble@fruitbook.local>
2023-02-27 13:36:45 -0700

    script-options

--- src/TabScripts.elm
+++ src/TabScripts.elm
@@ -11,38 +11,43 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Common exposing (..)
 
-
-type alias ScriptInfo =
+type alias ScriptInfo msg =
     { prettyName : String
     , desc : String
     , name : String -- internal name
     , classes : List Tf2.Class -- give me my fucking typeclasses back i beg. i wanted to use `Set`
+    , options : Html msg
     }
 
-scripts : List ScriptInfo
+scripts : List (ScriptInfo msg)
 scripts =
+    let odiv = div [ class "script-options" ] in
     [ ScriptInfo "Automatic Crouch Jump"
                  "automatically crouch whenever you jump"
                  "crouch-jump"
                  allClasses
+                 <| odiv []
     , ScriptInfo "Übercharge Alert"
-                 "send a message notifying your team whenever you use your Übercharge"
+                 "send a message notifying your team whenever you use Übercharge"
                  "uber-alert"
                  [Medic]
-    , ScriptInfo "Dropping is Bad"
+                 <| odiv []
+    , ScriptInfo "No-Drop"
                  "switch to your Medi-gun and drop the intelligence when you attempt to use Über"
                  "no-drop"
                  [Medic]
+                 <| odiv []
     , ScriptInfo "Quick Teleport"
                  "blah"
                  "quick-teleport"
                  [Engineer]
+                 <| odiv []
     ]
 
 tabHTML : Set String -> Html Msg
 tabHTML set = div [ class "scripts-container" ] <| List.map (viewScriptInfo set) scripts
 
-viewScriptInfo : Set String -> ScriptInfo -> Html Msg
+viewScriptInfo : Set String -> ScriptInfo Msg -> Html Msg
 viewScriptInfo scriptset sc =
     -- let idstr = String.fromInt <| sc.id in
     div
@@ -60,6 +65,7 @@ viewScriptInfo scriptset sc =
             [ p [ class "name" ] [ text sc.prettyName ]
             ]
         , p [ class "desc" ] [ text sc.desc ]
+        , sc.options
         ]
     ]
 
